6b731cce5734a5cc11a1f702623da38b6ef267a8
Sergey "GrayFace" Rozhenko <sergroj@mail.ru>
2022-12-23 14:11:55 +0700

    Show non-standard ObjList.txt objects by default

--- Scripts/Structs/After/Spells.lua
+++ Scripts/Structs/After/Spells.lua
@@ -262,17 +262,19 @@ if mmver > 6 then
 		test al, 3  ; is changed?
 		jz @std
 		test al, 2  ; hide spc effect?
-		jnz @f
+		jnz @show
 	;spc effect with SW over it:
 		push dword [esp+4]
 		call @std
 		mov eax, 1
 		ret 4
-	@@:
+	@show:
 	;no spc effect:
 		and eax, 1
 		ret 4
 	@std:
+		cmp edx, mm7*9120 + mm8*12120
+		jnb @show
 	]])
 end
 
