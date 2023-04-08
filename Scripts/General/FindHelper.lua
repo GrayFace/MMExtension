7be1db01c98418ae00af018505fd6c5802741a1d
Sergey "GrayFace" Rozhenko <sergroj@mail.ru>
2023-04-08 12:25:44 +0700

    Improved FR/FR1

--- Scripts/General/FindHelper.lua
+++ Scripts/General/FindHelper.lua
@@ -1,56 +1,61 @@
+local abs, floor, ceil, round, max, min = math.abs, math.floor, math.ceil, math.round, math.max, math.min
 local i4, i2, i1, u4, u2, u1, i8, u8 = mem.i4, mem.i2, mem.i1, mem.u4, mem.u2, mem.u1, mem.i8, mem.u8
 local format = string.format
 
-function FR(p, p1, start, stop)
+local function DoFR(p, p1, start, stop, sz)
 	p1 = p1 or p + 1
-	local t = {}
-	local t1 = {}
+	local q = {{}, {}, {}}
+	local a = sz == 4 and u4 or sz == 2 and u2 or sz == 1 and u1
 
-	local last
-	for i = (start or 0x401000), (stop or select(Game.Version - 5, 0x4B9000, 0x4D8000, 0x508000)) do
-		local n = mem.GetInstructionSize(i)
-		local _, IsJump = mem.GetHookSize(i)
+	local last, kind
+	local store = || if last then
+		local t = q[kind]
+		t[#t+1] = format("0x%X", last)
+	end
+	
+	local good = start or 0x401000
+	for i = good, (stop or select(Game.Version - 5, 0x4B9000, 0x4D8000, 0x508000)) do
+		local n, IsJump, _ = mem.GetInstructionSize(i)
+		if sz == 4 then
+			_, IsJump = mem.GetHookSize(i)
+		end
 		if not IsJump then
 			local op = u1[i]
-			for i = i + 1, i + n - 4 do
-				local v = u4[i]
+			for j = i + 1, i + n - sz do
+				local v = a[j]
 				if v >= p and v < p1 then
-					if i ~= last then
-						local t = (op ~= 0x68 and t or t1)  -- put "push XXX" in t1
-						t[#t+1], last = format("0x%X", i), i
+					local knd = i ~= good and 3 or op == 0x68 and 2 or 1  -- put "push XXX" in q[2], probably wrong ones in q[3]
+					if j ~= last then
+						store()
+						last, kind = j, knd
+					elseif knd < kind then
+						kind = knd
 					end
 				end
 			end
 		end
-	end	
-	return "Ref{".._G.table.concat(t, ", ").."}", "Ref{".._G.table.concat(t1, ", ").."}"
-end
-
-function FR1(p, p1, start, stop)
-	p1 = p1 or p + 1
-	local t = {}
-	local t1 = {}
-
-	local last
-	for i = (start or 0x401000), (stop or select(Game.Version - 5, 0x4B9000, 0x4D8000, 0x508000)) do
-		local n, IsJump = mem.GetInstructionSize(i)
-		if not IsJump then
-			local op = u1[i]
-			for i = i + 1, i + n - 1 do
-				local v = u1[i]
-				if v >= p and v < p1 then
-					if i ~= last then
-						local t = (op ~= 0x68 and t or t1)  -- put "push XXX" in t1
-						t[#t+1], last = format("0x%X", i), i
-					end
-				end
-			end
+		if i == good then
+			good = good + n
 		end
-	end	
-	return "Ref{".._G.table.concat(t, ", ").."}", "Ref{".._G.table.concat(t1, ", ").."}"
+	end
+	store()
+	return "Ref{"..table.concat(q[1], ", ").."}", "RefPush{"..table.concat(q[2], ", ").."}", "RefSus{"..table.concat(q[3], ", ").."}"
 end
 
---[=[-- Find all function calls:
+
+FR = |p, p1, start, stop| DoFR(p, p1, start, stop, 4)
+FR1 = |p, p1, start, stop| DoFR(p, p1, start, stop, 1)
+
+
+--[=[
+
+-- Find array refs:
+local a = Game.ItemsTxt
+print(FR(a.?ptr - a[1].?size, a.?ptr))
+print(FR(a.?ptr, a[a.High].?ptr))
+print(FR(a[a.High].?ptr, a[a.High].?ptr + a[1].?size*2))
+
+-- Find all function calls:
 local need = 0x402CAE
 local str = [[    (p: $%X; old: $402CAE; newp: @FastCosX; t: RShtCall; Querry: hqSmoothMouseLook), // Smooth mouse look]]
 local t = {}
